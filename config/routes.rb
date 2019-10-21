cab4eb001a6c8ef44e6ce3054771ea123e5b838c
EC2 Default User <ec2-user@ip-172-31-94-84.ec2.internal>
2019-10-21 17:37:38 +0900

    問い合わせページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+resources :contacts, only: [:new, :create]
+
   # get '/sign_up', to: 'users#sign_up', as: :sign_up
   # post '/sign_up', to: 'users#sign_up_process'
   get '/sign_in', to: 'users#sign_in', as: :sign_in
