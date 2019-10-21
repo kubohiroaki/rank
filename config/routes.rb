998302d30edbcb816c1e5a39b3518fc543047e76
EC2 Default User <ec2-user@ip-172-31-94-84.ec2.internal>
2019-10-21 10:02:55 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+
   get '/sign_up', to: 'users#sign_up', as: :sign_up
   post '/sign_up', to: 'users#sign_up_process'
   get '/sign_in', to: 'users#sign_in', as: :sign_in
   post '/sign_in', to: 'users#sign_in_process'
   get '/sign_out', to: 'users#sign_out', as: :sign_out
   resources :movies 
+    root 'movies#index' #トップページをmoviesコントローラのindexアクションに設定
     get '/search_result', to: 'movies#search_result'
     post 'movies/next', to: 'movies#next'
     post 'movies/next2', to: 'movies#next_story'
@@ -12,6 +14,7 @@ Rails.application.routes.draw do
     post 'movies/next4', to: 'movies#next_emotion'
     post 'movies/next5', to: 'movies#next_fiction'
     get 'like/(:id)', to: 'movies#like', as: :like 
+
   
   get '/top', to: 'ranking#top'
   get 'ranking/type', to: 'ranking#type', as: :type
