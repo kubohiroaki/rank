b9a060dfbec219d4e77f1b39200d673aec2859da
EC2 Default User <ec2-user@ip-172-31-94-84.ec2.internal>
2019-10-21 17:13:28 +0900

    ユーザーテーブルにカラム追加＋サインアップのパスを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  get '/sign_up', to: 'users#sign_up', as: :sign_up
-  post '/sign_up', to: 'users#sign_up_process'
+  # get '/sign_up', to: 'users#sign_up', as: :sign_up
+  # post '/sign_up', to: 'users#sign_up_process'
   get '/sign_in', to: 'users#sign_in', as: :sign_in
   post '/sign_in', to: 'users#sign_in_process'
   get '/sign_out', to: 'users#sign_out', as: :sign_out
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191020032617) do
+ActiveRecord::Schema.define(version: 20191021080424) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20191020032617) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
+    t.text "description"
   end
 
   add_foreign_key "movie_likes", "movies"
